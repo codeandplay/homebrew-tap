5ce0563c97a18e658fca361547c7c36247d616c9
codeandplay <raymac@MINGRUIs-MacBook-Pro.local>
2022-05-02 21:07:13 +0100

    Update sha

--- Formula/sss.rb
+++ Formula/sss.rb
@@ -2,7 +2,7 @@ class Sss < Formula
   desc "Command tool to keep your Ethereume HD wallet passphrase safe and secure"
   homepage "https://github.com/codeandplay/eth-passphrase-backup"
   url "https://github.com/codeandplay/eth-passphrase-backup/releases/download/v0.1.0/sss-0.1.0-x86_64-apple-darwin.tar.gz"
-  sha256 "01743aa7867bcc2a59df7fb62682a7e379ae57f9"
+  sha256 "8bb1c52f07e0bab4f23fbfe3d70a770bb3d62f74586cc83b578c3dcf7537295b"
   version "0.1.0"
 
   def install
